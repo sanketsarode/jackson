8742f5feddbb262048f6f9199fdb672a3f7ee157
Sanket Sarode <sanket.sarode41@gmail.com>
2020-05-29 10:17:16 +0530

    multiple ways of handling json and converting to java object

--- src/test/java/com/jackson/test/TestCars.java
+++ src/test/java/com/jackson/test/TestCars.java
@@ -1,20 +1,25 @@
 package com.jackson.test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.List;
+import java.util.Map;
 
 public class TestCars {
+
+    ObjectMapper objmapper = new ObjectMapper();
+    String car = null;
 
     /**
      * Simply Converting string to java object
      **/
     @Test
     public void MyCarsTest() {
-        ObjectMapper objmapper = new ObjectMapper();
 
-        String car = "{ \"brand\" : \"Tata\", \"doors\" : 5}";
+        car = "{ \"brand\" : \"Tata\", \"doors\" : 5}";
 
         try {
             Car c = objmapper.readValue(car, Car.class);
@@ -33,9 +38,8 @@ public class TestCars {
      */
     @Test
     public void MyCarsTestReader() {
-        ObjectMapper objmapper = new ObjectMapper();
 
-        String car = "{ \"brand\" : \"Hyundai\", \"doors\" : 5}";
+        car = "{ \"brand\" : \"Hyundai\", \"doors\" : 5}";
 
         Reader reader = new StringReader(car);
 
@@ -56,7 +60,6 @@ public class TestCars {
      */
     @Test
     public void MyCarsTestUsingFile() {
-        ObjectMapper objmapper = new ObjectMapper();
 
         File file = new File(System.getProperty("user.dir") + "/src/test/resources/first.json");
 
@@ -77,7 +80,6 @@ public class TestCars {
      */
     @Test
     public void MyCarsTestUsingInputStream() {
-        ObjectMapper objmapper = new ObjectMapper();
 
         try {
             InputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/first.json");
@@ -86,6 +88,69 @@ public class TestCars {
             System.out.println("Converting JSONFile to java object using InputStream");
             System.out.println("Car Brand: " + c.getBrand());
             System.out.println("Car Doors: " + c.getDoors());
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Converting JSON String to java array object
+     */
+    @Test
+    public void MyCarsTestJSONArrayString() {
+        car = "[{ \"brand\" : \"Fiat\", \"doors\" : 4},{ \"brand\" : \"Kia\", \"doors\" : 5}]";
+
+        try {
+            Car[] c = objmapper.readValue(car, Car[].class);
+
+            System.out.println("Converting JSON string to java array object");
+            for (Car s : c) {
+                System.out.println("Car Brand: " + s.getBrand());
+                System.out.println("Car Doors: " + s.getDoors());
+            }
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Converting JSON String to java list object
+     */
+    @Test
+    public void MyCarsTestJSONListString() {
+        car = "[{ \"brand\" : \"Kia\", \"doors\" : 4},{ \"brand\" : \"Fiat\", \"doors\" : 5}]";
+
+        try {
+            List<Car> c = objmapper.readValue(car, new TypeReference<List<Car>>() {
+            });
+
+            System.out.println("Converting JSON string to java list object");
+            for (Car s : c) {
+                System.out.println("Car Brand: " + s.getBrand());
+                System.out.println("Car Doors: " + s.getDoors());
+            }
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Converting JSON String to java Map Object
+     */
+    @Test
+    public void MyCarsTestJSONMapObject() {
+        car = "{ \"brand\" : \"Hyundai\", \"doors\" : 5}";
+
+        try {
+            Map<String, Object> c = objmapper.readValue(car, new TypeReference<Map<String, Object>>() {
+            });
+
+            System.out.println("Converting JSON string to java map object");
+            System.out.println("Car Brand: " + c.get("brand"));
+            System.out.println("Car Doors: " + c.get("doors"));
             System.out.println();
         } catch (Exception e) {
             e.printStackTrace();
